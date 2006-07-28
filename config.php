22194713f0d6b6a552816e2a4dbe6d6039c4051d
jvyyuie <jvyyuie@a27e6123-9f19-0410-975c-41362c082a96>
2006-07-28 09:15:13 +0000

    基本的配置文件

---
+++ config.php
@@ -0,0 +1,13 @@
+<?
+	# 2006-7-28 17:10 by Maxview
+	
+	// Database Config
+	$dbserver="localhost";
+	$dbserver="root";
+	$dbserver="";
+	
+	// Website Config
+	$title="无聊者的网络社区";
+	$title_sig="日出东海落西山";
+	$website="http://www.bepet.net";
+?>
